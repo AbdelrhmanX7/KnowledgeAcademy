24c93046c4804a3fec0a78d533c48b005ce12623
Abdelrhman Kamal <hadh2819@gmail.com>
2023-11-19 07:00:51 +0200

    Remove unused files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import HomeContent from "@/components/Home";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <HomeContent />
-    </>
-  );
+  return <HomeContent />;
 }
