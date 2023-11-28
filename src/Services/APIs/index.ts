dd378fc07b1034c445c8511b4677fe98bde212fd
Abdelrhman Kamal <hadh2819@gmail.com>
2023-11-28 03:32:19 +0200

    Add balance

--- src/Services/APIs/index.ts
+++ src/Services/APIs/index.ts
@@ -1,6 +1,7 @@
-export const API = process.env.API || 'http://localhost:4000';
+export const API = 'http://192.168.1.2:4000';
 
 export * from './Authentication';
+export * from './EWallet';
 //
 //export const API = "https://luxuriant-drawer-production.up.railway.app"; // development API
 //export const API = "http://196.221.166.103:8080";
