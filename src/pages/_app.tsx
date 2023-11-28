990affbdfdc58354fcd2ab738975ab6acd0ad1bf
mohamed-12-abdelkader <mharedy1212@gmail.com>
2023-11-28 04:29:06 -0800

    save

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,31 +6,34 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import axios from 'axios';
 import { getCookie } from 'cookies-next';
 import { Toaster } from 'react-hot-toast';
-import { ConfigProvider } from 'antd';
+import UserProvider from '@/context/Context';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   const token = getCookie('token');
   axios.defaults.headers.common.Authorization = token;
+
   return (
-    <ConfigProvider direction='rtl'>
-      <QueryClientProvider client={queryClient}>
-        <Toaster
-          toastOptions={{
-            style: {
-              borderRadius: '10px',
-              background: 'black',
-              color: '#fff',
-              fontSize: '18px',
-              fontWeight: 'semibold',
-            },
-          }}
-          position='bottom-right'
-        />
+    <QueryClientProvider client={queryClient}>
+      <Toaster
+        toastOptions={{
+          style: {
+            borderRadius: '10px',
+            background: 'black',
+            color: '#fff',
+            fontSize: '18px',
+            fontWeight: 'semibold',
+          },
+        }}
+        position='bottom-right'
+      />
+      <UserProvider>
         <Navbar />
+
         <Component {...pageProps} />
+
         <Footer />
-      </QueryClientProvider>
-    </ConfigProvider>
+      </UserProvider>
+    </QueryClientProvider>
   );
 }
