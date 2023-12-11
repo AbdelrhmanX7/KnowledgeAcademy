73518427779de9a7b8045257b0f223fa205c32a4
mohamed-12-abdelkader <mharedy1212@gmail.com>
2023-12-11 00:14:00 -0800

    edit card 2

--- src/UI/card/index.tsx
+++ src/UI/card/index.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from 'react';
-
+import { TiPinOutline } from 'react-icons/ti';
 import Link from 'next/link';
 import { RiFolderVideoFill } from 'react-icons/ri';
+
 interface CardProps {
   title: string;
   img: string;
@@ -20,7 +21,7 @@ export const Card: React.FC<Partial<CardProps>> = ({
   img = 'DEFAULT_IMAGE_URL',
   type,
   children,
-  href,
+  href: href = '', // Add a default value
   Class,
   Subject,
   number,
@@ -35,24 +36,29 @@ export const Card: React.FC<Partial<CardProps>> = ({
     );
   } else if (type === 'teacher') {
     return (
-      <div className='w-[250px] h-[350px] m-5 rounded-lg border shadow my-8 overflow-hidden flex flex-col'>
-        <div className='h-[80%] relative overflow-hidden'>
-          <img src={img} alt={title} className='w-full h-full object-cover h-[100%]' />
+      <div className='relative w-[280px] h-[auto] m-5 rounded-lg border bg-blue-500 shadow-xl my-8 overflow-hidden flex flex-col'>
+        <div className='absolute top-0 right-0 p-2'>
+          <TiPinOutline className='text-red-500' style={{ fontSize: '30px' }} />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <div className='h-[180px] w-[180px] relative overflow-hidden mt-3' style={{ borderRadius: '50%' }}>
+            <img src={img} alt={title} className='w-[100%] h-full object-cover h-[100%]' />
+          </div>
         </div>
         <div className='p-4 flex flex-col justify-center flex-1' style={{ direction: 'rtl' }}>
           <div className=''>
-            <div className='flex ' style={{ justifyContent: 'space-between' }}>
-              <p className='text-blue-500'> {Subject}</p>
-              <p className='text-blue-500'> {name}</p>
+            <div className='text-center flex' style={{ justifyContent: 'space-between' }}>
+              <h3 className='text-white'> {name}</h3>
+              <h4 className='text-black m-2'> {Subject}</h4>
             </div>
-            <p>{description}</p>
+            <p style={{ fontSize: '17px', fontWeight: 'bold', color: 'white' }}>{description}</p>
           </div>
         </div>
       </div>
     );
   } else if (type === 'lecturer') {
     return (
-      <Link href={href ?? ''}>
+      <Link href={href}>
         <div className='w-[250px] h-[350px] m-5 rounded-lg border shadow my-8 overflow-hidden flex flex-col'>
           <div className='h-[70%] relative overflow-hidden'>
             <img src={img} alt={title} className='w-full h-full object-cover' />
