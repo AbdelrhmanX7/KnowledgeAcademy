afa332354d43c015fd8294fa7c87a05f097639a3
Abdelrhman Kamal <hadh2819@gmail.com>
2023-12-13 03:45:25 +0200

    Revert "edit card" (#27)

--- src/UI/card/index.tsx
+++ src/UI/card/index.tsx
@@ -1,83 +1,57 @@
 import React, { ReactNode } from 'react';
-import { TiPinOutline } from 'react-icons/ti';
+import { Button } from '@/UI';
+import Image from 'next/image';
 import Link from 'next/link';
-import { RiFolderVideoFill } from 'react-icons/ri';
-
 interface CardProps {
   title: string;
   img: string;
   type: string;
   children: ReactNode;
   href: string;
-  Class: string;
-  Subject: string;
-  number: number;
-  description: string;
-  name: string;
 }
 
-export const Card: React.FC<Partial<CardProps>> = ({
-  title,
-  img = 'DEFAULT_IMAGE_URL',
-  type,
-  children,
-  href: href = '', // Add a default value
-  Class,
-  Subject,
-  number,
-  description,
-  name,
-}) => {
+export const Card: React.FC<Partial<CardProps>> = ({ title, img, type, children, href }) => {
   if (type === 'description') {
     return (
-      <div className=' h-[auto] w-[100%] rounded-lg border shadow-lg my-8 px-3 py-2  '>
+      <div className=' h-[400px] w-[600px] rounded-lg border shadow-lg my-8 px-3 py-2 hover:scale-110 transition duration-500'>
         {children ? children : null}
       </div>
     );
-  } else if (type === 'teacher') {
+  } else if (type == 'classe') {
     return (
-      <div className='relative w-[280px] h-[auto] m-5 rounded-lg border bg-blue-500 shadow-xl my-8 overflow-hidden flex flex-col'>
-        <div className='absolute top-0 right-0 p-2'>
-          <TiPinOutline className='text-red-500' style={{ fontSize: '30px' }} />
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <div className='h-[180px] w-[180px] relative overflow-hidden mt-3' style={{ borderRadius: '50%' }}>
-            <img src={img} alt={title} className='w-[100%] h-full object-cover h-[100%]' />
-          </div>
-        </div>
-        <div className='p-4 flex flex-col justify-center flex-1' style={{ direction: 'rtl' }}>
-          <div className=''>
-            <div className='text-center flex' style={{ justifyContent: 'space-between' }}>
-              <h3 className='text-white'> {name}</h3>
-              <h4 className='text-black m-2'> {Subject}</h4>
-            </div>
-            <p style={{ fontSize: '16px', fontWeight: 'bold', color: 'white' }}>{description}</p>
-          </div>
+      <div className=' w-[320px]  m-5 rounded-lg border shadow-lg my-8 px-3 py-2 hover:scale-110 transition duration-500'>
+        <div>
+          <img src={img} className='h-[320px]' />
+          <Link href={href ?? ''}>
+            <Button className='w-[100%]'>{title}</Button>
+          </Link>
         </div>
       </div>
     );
-  } else if (type === 'lecturer') {
+  } else if (type === 'lecture') {
     return (
-      <Link href={href}>
-        <div className='w-[250px] h-[350px] m-5 rounded-lg border shadow my-8 overflow-hidden flex flex-col'>
-          <div className='h-[70%] relative overflow-hidden'>
-            <img src={img} alt={title} className='w-full h-full object-cover' />
+      <div className=' w-[320px]  m-5 rounded-lg border shadow-lg my-8 px-3 py-2 hover:scale-110 transition duration-500'>
+        <div>
+          <div className='text-right flex' style={{ justifyContent: 'space-between' }}>
+            <h4 className='m-1'>
+              {' '}
+              السعر :<span style={{ color: 'red' }}> 150 جنية </span>{' '}
+            </h4>
+            <h4 className='m-1'>{title}</h4>
           </div>
-          <div className='p-4 flex flex-col justify-center flex-1' style={{ direction: 'rtl' }}>
-            <div className=''>
-              <div className='flex' style={{ justifyContent: 'space-between' }}>
-                <h1 className='text-sm text-gray-500'> {Class}</h1>
-                <h1 className='text-sm text-blue-500'> {Subject}</h1>
-              </div>
-              <h2 className='text-lg font-semibold mb-2'>{title}</h2>
-              <p className='flex items-center text-sm '>
-                <RiFolderVideoFill className='m-1 text-red-500' />
-                {number} محاضرات
-              </p>
-            </div>
+          <p className='w-[100%] mx-auto bg-blue-500 h-0.5'></p>
+          {typeof img === 'string' ? (
+            <img src={img} alt={title} /> // Regular image
+          ) : (
+            <Image src={img ?? ''} alt={title ?? ''} style={{ height: '300px' }} /> // Image loaded using next/image
+          )}
+          <p className='w-[100%] mx-auto bg-blue-500 h-0.5'></p>
+          <div className='flex justify-between'>
+            <Button className='m-3'> اشترك الان </Button>
+            <Button className='m-3'> دخول للكورس </Button>
           </div>
         </div>
-      </Link>
+      </div>
     );
   }
 };
