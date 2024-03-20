a4c2c9b5aad0d08a831b4b82f3274606928de1bb
Abdelrhman Kamal <hadh2819@gmail.com>
2024-03-20 19:47:53 +0200

    Remove services

--- src/Services/APIs/index.ts
+++
@@ -1,9 +0,0 @@
-export const API = process.env.API ?? 'https://luxuriant-drawer-production.up.railway.app';
-
-export * from './authentication';
-export * from './e-wallet';
-export * from './upload';
-//
-//export const API = "https://luxuriant-drawer-production.up.railway.app"; // development API
-//export const API = "http://196.221.166.103:8080";
-//export const API = "http://192.168.1.2:4000";
