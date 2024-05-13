8579fe2b7a282d05224a59f72fb7bb28e8f14c46
Abdelrhman Kamal <hadh2819@gmail.com>
2024-05-13 04:02:53 +0300

    restore

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const STUDY_PHASES = [
+  {
+    label: 'الصف الاول الثانوي',
+    value: '1',
+  },
+  {
+    label: 'الصف الثاني الثانوي',
+    value: '2',
+  },
+  {
+    label: 'الصف الثالث الثانوي',
+    value: '3',
+  },
+];
+
+export * from './subjects';
+export * from './user-default-data';
+export * from './tableHeader';
---
+++ src/constants/user-default-data.ts
@@ -0,0 +1,15 @@
+import { User } from '@/hooks/type';
+
+export const DEFAULT_USER_DATA: User = {
+  _id: '',
+  username: '',
+  email: '',
+  phone: '',
+  firstName: '',
+  lastName: '',
+  password: '',
+  studyPhase: '',
+  role: '',
+  createdAt: '',
+  __v: 0,
+};
