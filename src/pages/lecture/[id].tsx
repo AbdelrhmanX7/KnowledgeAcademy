1e32958e5cecfee9516d99fee67e2ade0863934a
Abdelrhman Kamal <hadh2819@gmail.com>
2024-05-20 03:20:30 +0300

    try to fix video issues

--- src/pages/lecture/[id].tsx
+++ src/pages/lecture/[id].tsx
@@ -2,7 +2,7 @@ import { useGetLecture } from '@/services/hooks';
 import { useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
-
+import ReactPlayer from 'react-player';
 export default function LectureId() {
   const queryClient = useQueryClient();
   useEffect(() => {
@@ -10,12 +10,16 @@ export default function LectureId() {
   }, []);
   const { query } = useRouter();
   const { data } = useGetLecture({ id: query?.id as string });
+  console.log(data);
   return (
     <div>
       {data?.videoUrl && (
-        <video id='videoPlayer' width='1000' controls>
-          <source src={data.videoUrl} type='video/mp4' />
-        </video>
+        <>
+          <video id='videoPlayer' width='1000' controls>
+            <source src={data.videoUrl} type='video/mp4' />
+          </video>
+          <ReactPlayer playsinline controls url={data.videoUrl} />
+        </>
       )}
     </div>
   );
